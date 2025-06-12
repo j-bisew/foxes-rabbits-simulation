e5f68228421cd1f3f685e8d09caf93aae2415213
Julia Bisewska <bisewskaj@gmail.com>
2025-06-12 11:17:51 +0200

    Added figures

---
+++ geom/figures.go
@@ -0,0 +1,22 @@
+package figures
+
+type Point struct {
+	X, Y float64
+}
+
+type Rectangle struct {
+	X, Width, Y, Height float64
+}
+
+func NewRectangle(x, width, y, height float64) *Rectangle {
+	return &Rectangle{x, width, y, height}
+}
+
+func (r *Rectangle) Contains(p Point) bool {
+	return p.X >= r.X && p.X <= r.X+r.Width && p.Y >= r.Y && p.Y <= r.Y+r.Height
+}
+
+func (r *Rectangle) Intersects(other Rectangle) bool {
+	return !(r.X > other.X+other.Width || r.X+r.Width < other.X ||
+	r.Y > other.Y+other.Height || r.Y+r.Height < other.Y)
+}
