692019576e051e5fa1cdbe7cf039133ae6aa5833
Julia Bisewska <bisewskaj@gmail.com>
2025-06-12 11:22:17 +0200

    main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/j-bisew/foxes-rabbits-simulation
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// var width = flag.Float64("w", 400, "Board width")
+	// var height = flag.Float64("h", 400, "Board height")
+
+	flag.Parse()
+	fmt.Println("Ecosystem Simulation Starting...")
+}
