e142babc0064ff93d9e77f508d22d9620497a1a6
Julia Bisewska <bisewskaj@gmail.com>
2025-06-17 22:16:44 +0200

    new methods in quadtree

--- quadtree/quadTree.go
+++ quadtree/quadTree.go
@@ -5,16 +5,23 @@ import (
 	"strings"
 
 	"github.com/j-bisew/foxes-rabbits-simulation/geom"
+	"github.com/j-bisew/foxes-rabbits-simulation/interfaces"
 )
 
 
 type Point = geom.Point
 type Rectangle = geom.Rectangle
+type Entity = interfaces.Entity
+
+type EntityPoint struct {
+	Point Point
+	Entity Entity
+}
 
 type QuadTree struct {
 	capacity int
 	boundary Rectangle
-	points []Point
+	points []EntityPoint
 	nw,ne,sw,se *QuadTree
 	divided bool
 }
@@ -23,7 +30,7 @@ func NewQuadTree(capacity int, rect Rectangle) *QuadTree {
 	return &QuadTree{
 		capacity: capacity,
 		boundary: rect,
-		points:   make([]Point, 0, capacity),
+		points:   make([]EntityPoint, 0, capacity),
 		divided:  false,
 	}
 }
@@ -41,43 +48,51 @@ func (qt *QuadTree) subdivide() {
 
 	qt.divided = true
 
-	for _, point := range qt.points {
-		_ = qt.ne.Insert(point) ||
-		qt.nw.Insert(point) ||
-		qt.se.Insert(point) ||
-		qt.sw.Insert(point)
+	for _, ep := range qt.points {
+		_ = qt.ne.Insert(ep.Point, ep.Entity) ||
+		qt.nw.Insert(ep.Point, ep.Entity) ||
+		qt.se.Insert(ep.Point, ep.Entity) ||
+		qt.sw.Insert(ep.Point, ep.Entity)
 	}
 
 	qt.points = nil
 }
 
-func (qt *QuadTree) Insert(point Point) bool {
+func (qt *QuadTree) Clear() {
+	qt.points = qt.points[:0]
+	qt.divided = false
+	qt.ne, qt.nw, qt.se, qt.sw = nil, nil, nil, nil
+}
+
+func (qt *QuadTree) Insert(point Point, entity Entity) bool {
 	if !qt.boundary.Contains(point) {
 		return false
 	}
 
+	entityPoint := EntityPoint{Point: point, Entity: entity}
+
 	if !qt.divided {
 		if len(qt.points) < qt.capacity {
-			qt.points = append(qt.points, point)
+			qt.points = append(qt.points, entityPoint)
 			return true
 		}
 		qt.subdivide()
 	}
-	return qt.ne.Insert(point) ||
-		qt.nw.Insert(point) ||
-		qt.se.Insert(point) ||
-		qt.sw.Insert(point)
+	return qt.ne.Insert(point, entity) ||
+		qt.nw.Insert(point, entity) ||
+		qt.se.Insert(point, entity) ||
+		qt.sw.Insert(point, entity)
 }
 
-func (qt *QuadTree) Query(rangeRect Rectangle, found *[]Point) {
+func (qt *QuadTree) Query(rangeRect Rectangle, found *[]Entity) {
 	if !qt.boundary.Intersects(rangeRect) {
 		return
 	}
 	
 	if !qt.divided {
-		for _, p := range qt.points {
-			if rangeRect.Contains(p) {
-				*found = append(*found, p)
+		for _, ep := range qt.points {
+			if rangeRect.Contains(ep.Point) {
+				*found = append(*found, ep.Entity)
 			}
 		}
 		return
@@ -102,7 +117,7 @@ func (qt *QuadTree) stringWithIndent(indent string) string {
 		if len(qt.points) > 0 {
 			sb.WriteString(fmt.Sprintf(" points:%d\n", len(qt.points)))
 			for i, point := range qt.points {
-				sb.WriteString(fmt.Sprintf("%s [%d] (%.1f, %.1f)\n", indent, i, point.X, point.Y))
+				sb.WriteString(fmt.Sprintf("%s [%d] (%.1f, %.1f)\n", indent, i, point.Point.X, point.Point.Y))
 			}
 		} else {
 			sb.WriteString(" (empty)\n")
